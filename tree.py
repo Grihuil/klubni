121402d8825cc61d1639b31a5617aafa0b5ad876
Grihuil <128086642+Grihuil@users.noreply.github.com>
2023-03-19 04:11:40 +0300

    Add files via upload

---
+++ tree.py
@@ -0,0 +1,10 @@
+import os
+
+def tree(path, level=0):
+    print('|  '*level, end='')
+    print('|--', os.path.basename(path))
+    if os.path.isdir(path):
+        for file in os.listdir(path):
+            tree(os.path.join(path, file), level+1)
+
+tree('.')
